0bd96a1afbc7b425048c324f4c5f89193c940a7b
Hemang Mehta <61517187+Hemang-05@users.noreply.github.com>
2024-12-26 19:18:55 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -141,7 +141,7 @@ async function submitForm() {
     };
 
     try {
-        const BACKEND_URL = 'http://localhost:5000/api/form';
+        const BACKEND_URL = 'https://formsupp-be.onrender.com/api/form';
 
         // Example API call
         const response = await fetch(BACKEND_URL, {
